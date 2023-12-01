1bd3adb4527089086cecca75c3f516130a885e0a
Airel Camilo Khairan <airel.camilo@ui.ac.id>
2023-12-01 23:13:59 +0700

    feat: add auth to Makanan and sort Makanan based on nama

--- makanan/urls.py
+++ makanan/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from makanan.views import getDaftarMakanan, addMakananSaya
+from makanan.views import GetDaftarMakananAPI, AddMakananSayaAPI
 
 app_name = 'makanan'
 
 urlpatterns = [
-    path('all/<int:userId>', getDaftarMakanan, name='getDaftarMakanan'),
-    path('add/', addMakananSaya, name='addMakananSaya'),
+    path('all', GetDaftarMakananAPI.as_view(), name='get_daftar_makanan'),
+    path('add', AddMakananSayaAPI.as_view(), name='add_makanan_saya'),
 ]
