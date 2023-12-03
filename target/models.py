f4b1af4fcd56c54c72b74264f479f19b9e66f7ea
rahmaadnda <putrirahmaadinda@gmail.com>
2023-12-03 14:43:27 +0700

    feat: add additional field for target

--- target/models.py
+++ target/models.py
@@ -6,4 +6,5 @@ class Target(models.Model):
     userId = models.IntegerField()
     tujuan = models.CharField(max_length=30)
     jangkaWaktu = models.IntegerField()
+    perubahanBeratBadan = models.IntegerField(default=None, null=True, blank=True)
     targetKaloriHarian = models.FloatField()
